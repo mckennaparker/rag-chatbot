c4493d64dc4c224a7f4ad7bf863a2e355dbfdfe0
McKenna P <152559424+mckennaparker@users.noreply.github.com>
2025-08-04 12:26:05 -0400

    Completed RAG Functions

--- functions/rag_mlh.py
+++ functions/rag_mlh.py
@@ -118,4 +118,46 @@ def upsert_data_to_pinecone(data_with_metadata: list[dict[str, any]]) -> None:
     index.upsert(vectors=data_with_metadata)
 
 #Call the function
-upsert_data_to_pinecone(data_with_metadata=data_with_meta_data)
+# upsert_data_to_pinecone(data_with_metadata=data_with_meta_data)
+
+def get_query_embeddings(query: str) -> list[float]:
+    query_embeddings = EMBEDDINGS.embed_query(query)
+    return query_embeddings
+
+# Call the function
+prompt = "What is MLH"
+query_embeddings = get_query_embeddings(query=prompt)
+
+def query_pinecone_index(
+    query_embeddings: list, top_k: int = 2, include_metadata: bool = True
+) -> dict[str, any]:
+    query_response = index.query(
+        vector=query_embeddings, top_k=top_k, include_metadata=include_metadata
+    )
+    return query_response
+
+# Call the function
+answers = query_pinecone_index(query_embeddings=query_embeddings)
+print(answers)
+
+from openai import OpenAI
+OPENAI_API_KEY= st.secrets["OPENAI_API_KEY"]
+def generate_answer(answers: dict[str, any]) -> str:
+  client = OpenAI(api_key=OPENAI_API_KEY)
+  text_content = answers['matches'][0]['metadata']['text']
+
+  completion = client.chat.completions.create(
+      model="gpt-4o",
+      messages=[
+          {"role": "developer", "content": text_content},
+          {
+              "role": "user",
+              "content": "With the given context provide a better answer to the question: " + prompt,
+
+          }
+      ]
+  )
+
+  print(completion.choices[0].message)
+
+generate_answer(answers)
